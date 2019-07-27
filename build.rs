7bc4d29619b2d393182abb61118f8b40fb2cf773
Noam Kleinburd <noam93k@gmail.com>
2019-07-27 22:45:24 +0300

    Improve compilation.

--- build.rs
+++ build.rs
@@ -11,10 +11,21 @@ use foreman::{LibKind, SearchKind};
 
 fn main() {
     let out_dir = foreman::out_dir().unwrap();
+    let host = foreman::host().unwrap();
     let valgrind_dir = out_dir.join("valgrind");
     let vex_dir = valgrind_dir.join("VEX");
 
     {
+        let (arch, platform) = {
+            let mut host_parts = host.as_str().split("-");
+            let arch = host_parts.next().unwrap();
+            let arch = if arch == "x86_64" { "amd64" } else { arch };
+            let _ = host_parts.next();
+            let platform = host_parts.next().unwrap();
+
+            (arch, platform)
+        };
+
         // Copy and build valgrind in OUT_DIR
         let options = CopyOptions { overwrite: true, skip_exist: true, buffer_size: 1<<16 };
         copy("valgrind", &out_dir, &options).unwrap();
@@ -32,17 +43,8 @@ fn main() {
         Command::new("make").args(&["-j", &format!("{}", foreman::num_jobs().unwrap())])
             .current_dir(&vex_dir)
             .status().unwrap();
-    }
 
-    {
         // Tell rustc to link to libvex
-        let host = foreman::host().unwrap();
-        let mut host_parts = host.as_str().split("-");
-        let arch = host_parts.next().unwrap();
-        let arch = if arch == "x86_64" { "amd64" } else { arch };
-        let _ = host_parts.next();
-        let platform = host_parts.next().unwrap();
-
         foreman::link_search(SearchKind::Native, &vex_dir);
         foreman::link_lib(LibKind::Static, &format!("vex-{}-{}", arch, platform));
     }
